2ee582b47029a3583536b38aa77dc2c03c29712a
adrycki <adriannrozycki@gmail.com>
2019-10-21 17:00:11 +0200

    Change formatting a little bit and fixed throwing Exception.

--- src/main/java/calculator/Lab.java
+++ src/main/java/calculator/Lab.java
@@ -1,54 +1,54 @@
 package calculator;
-//TODO ulepszenie do operacji na kilku liczb
-import java.util.*;
+
 import java.util.regex.Pattern;
+
+//TODO ulepszenie do operacji na kilku liczb
 public class Lab implements Ilab {
 
-    public double calculate(String str)  {
+
+    public double calculate(String str) throws Exception {
+
         String[] stringArray = str.split("");
-        String n1 = "";
-        String n2 = "";
+        String number1 = "";
+        String number2 = "";
         String sign = "";
         int index = 0;
 
         for (String a: stringArray) {
             if (Pattern.matches("[0-9.]", a)) {
                 if (index == 0) {
-                    n1 += a;
+                    number1 += a;
                 }
                 else {
-                    n2+=a;
+                    number2+=a;
                 }
             }
-            else if (Pattern.matches(" ", a)){}
+            else if (Pattern.matches(" ", a)){
+                throw new Exception();
+            }
             else if (Pattern.matches("[a-zA-Z]", a)) {
-                //throw Exception;
+
             } else if(Pattern.matches("[+/*-]", a)){
                 sign = a;
                 index++;
             }
         }
-        System.out.println(n1 + "<N1 n2>"+n2+"<>"+sign);
 
 
-        if(sign.equals("+"))
-        {
-            return Double.parseDouble(n1) + Double.parseDouble(n2);
+        if(sign.equals("+")) {
+            return Double.parseDouble(number1) + Double.parseDouble(number2);
         }
         
-        else if(sign.equals("-"))
-        {
-            return Double.parseDouble(n1) - Double.parseDouble(n2);
+        if(sign.equals("-")) {
+            return Double.parseDouble(number1) - Double.parseDouble(number2);
         }
         
-        else if(sign.equals("*"))
-        {
-            return Double.parseDouble(n1) * Double.parseDouble(n2) ;
+        if(sign.equals("*")) {
+            return Double.parseDouble(number1) * Double.parseDouble(number2) ;
         }
         
-        else if(sign.equals("/"))
-        {
-            return Double.parseDouble(n1) / Double.parseDouble(n2);
+        if(sign.equals("/")) {
+            return Double.parseDouble(number1) / Double.parseDouble(number2);
         }
 
             return 0;  //score
